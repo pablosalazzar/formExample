b925d7be2bc9e79e65565588e2301674061f813c
Pablo Salazar <salazarpabloale@gmail.com>
2026-06-24 17:03:55 -0600

    adding functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("");
+  const [email,setEmail]= useState("");
+  const [edad, setEdad]= useState(0);
+
+  function handleNameChange (e){
+    const currentValue = e.target.value;
+    setName(currentValue)
+  };
+
+  function handleEmailChange (e){
+    const currentValue = e.target.value;
+    setEmail(currentValue);
+  };
+
+  function handleEdadChange (e){
+    const currentValue = e.target.value;
+    setEdad(currentValue)
+  };
+  
 
   return (
     <>
-    <h1> User form </h1>
+      <h1> User form </h1>
 
-    <form >
-      <p>Name</p>
-      <input type="text" />
+      <form>
+        <p>Name</p>
+        <input type="text"  onChange={(handleNameChange)}/>
 
-      <p>Email</p>
-      <input type="text" />
+        <p>Email</p>
+        <input type="text" onChange={(handleEmailChange)} />
 
-      <p>Edad</p>
-      <input type="text" />
-    </form>
+        <p>Edad</p>
+        <input type="text"  onChange={(handleEdadChange)} />
 
-    <table>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Edad</th>
-      </tr>
-    </table>
+        <div>
+          <button>Add user</button>
+        </div>
+      </form>
+
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Edad</th>
+        </tr>
+      </table>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
